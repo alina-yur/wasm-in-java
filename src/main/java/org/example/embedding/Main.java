a054f9ad65d8ca3e1684049bb9882f261b7e2bac
alina-yur <alina.yurenko@oracle.com>
2024-05-28 15:48:42 +0200

    move to wasm

--- src/main/java/org/example/embedding/Main.java
+++ src/main/java/org/example/embedding/Main.java
@@ -55,33 +55,16 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         try (Context context = Context.newBuilder().allowAllAccess(true).build()) {
-            Set<String> languages = context.getEngine().getLanguages().keySet();
-            for (String id : languages) {
-                System.out.println("Initializing language " + id);
-                context.initialize(id);
-                switch (id) {
-                    case "python":
-                        context.eval("python", "print('Hello Python!')");
-                        break;
-                    case "js":
-                        context.eval("js", "print('Hello JavaScript!');");
-                        break;
-                    case "ruby":
-                        context.eval("ruby", "puts 'Hello Ruby!'");
-                        break;
-                    case "wasm":
-                        // with wasm we compute factorial
-                        context.eval(Source.newBuilder("wasm", Main.class.getResource("factorial.wasm")).name("main").build());
-                        Value factorial = context.getBindings("wasm").getMember("main").getMember("fac");
-                        System.out.println("wasm: factorial(20) = " + factorial.execute(20L));
-                        break;
-                    case "java":
-                        // with Java we invoke System.out.println reflectively.
-                        Value out = context.getBindings("java").getMember("java.lang.System").getMember("out");
-                        out.invokeMember("println", "Hello Espresso Java!");
-                        break;
-                }
-            }
+
+            System.out.println("Initializing wasm");
+            
+            context.initialize("wasm");
+
+                context.eval(Source.newBuilder("wasm", Main.class.getResource("factorial.wasm")).build());
+                System.out.println("Available members are: " + context.getBindings("wasm").getMemberKeys());
+                Value factorial = context.getBindings("wasm").getMember("factorial.wasm").getMember("fac");
+                System.out.println("wasm: factorial(20) = " + factorial.execute(20L));
+                
         }
     }
 }
